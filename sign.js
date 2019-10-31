a4a5e8a5b29b8ef3894e26bf39d890d36587ff47
Afonso Santos <55449265+afosantos1@users.noreply.github.com>
2019-10-30 23:26:31 -0400

    Update sign.js

--- sign.js
+++ sign.js
@@ -1,1 +1,17 @@
+ 
+const readlineSync = require("readline-sync");
 
+ const number = Number(readlineSync.question("\nEnter a number: "));
+
+if (number > 0){
+  console.log("\nPositive.\n");
+}
+else if (number < 0){
+  console.log("\nNegative.\n");
+}
+else if (number == 0){
+  console.log("\nZero.\n");
+}
+else if (Number.isNaN(number)){
+  console.log("\nInvalid.\n");
+}
