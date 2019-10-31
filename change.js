f506d186d0f4e881db8f90f835dce909d8834ae3
Afonso Santos <55449265+afosantos1@users.noreply.github.com>
2019-10-31 10:02:43 -0400

    Update change.js

--- change.js
+++ change.js
@@ -19,23 +19,28 @@ if (Number.isNaN(amount)) {
 } else if (amount < MIN || amount > MAX) {
     console.log("\nInvalid.");
 } else {
-    
+
     while (amount/VALUE_OF_QUARTERS >= 1 ){
       quarters = Math.floor(amount/VALUE_OF_QUARTERS);
       amount = amount % VALUE_OF_QUARTERS;
     }
+
     while (amount/VALUE_OF_DIMES >= 1){
       dimes = Math.floor(amount/VALUE_OF_DIMES);
       amount = amount % VALUE_OF_DIMES;
     }
+
+
     while (amount/VALUE_OF_NICKELS >= 1){
       nickels = Math.floor(amount/VALUE_OF_NICKELS);
       amount = amount % VALUE_OF_NICKELS;
     }
+
     while (amount/VALUE_OF_PENNY >= 1){
       pennies = Math.floor(amount/VALUE_OF_PENNY);
       amount = amount % VALUE_OF_PENNY;
     }
+
     console.log(`\n${quarters} quarters. ${dimes} dimes, ${nickels} nickels, and ${pennies} pennies.`);
 
 }
