4b67d2be9bd73a58eaf053a7dfb17825484a7a76
Afonso Santos <55449265+afosantos1@users.noreply.github.com>
2019-10-31 21:57:57 -0400

    Update parity.js

--- parity.js
+++ parity.js
@@ -1,1 +1,18 @@
+const readlineSync = require("readline-sync");
 
+const MIN = Number.MIN_SAFE_INTEGER;
+const MAX = Number.MAX_SAFE_INTEGER;
+
+const integer = Number(readlineSync.question("\nEnter an integer: "));
+
+if (Number.isNaN(integer)) {
+    console.log("\nInvalid.");
+} else if (integer < MIN || integer > MAX) {
+    console.log("\nInvalid.");
+} else if ((integer - Math.floor(integer))> 0) {
+    console.log("\nInvalid.");
+} else if (integer % 2 === 0) {
+    console.log("\nEven.");
+} else {
+    console.log("\nOdd.");
+}
