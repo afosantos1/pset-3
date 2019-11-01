b3f972ccf3f0d5c269840690ca21060c96392a66
Afonso Santos <55449265+afosantos1@users.noreply.github.com>
2019-10-31 22:14:23 -0400

    Update sequence.js

--- sequence.js
+++ sequence.js
@@ -1,1 +1,27 @@
+const readlineSync = require("readline-sync");
 
+const MIN = Number.MIN_SAFE_INTEGER;
+const MAX = Number.MAX_SAFE_INTEGER;
+let numberOne = Number(readlineSync.question("\nEnter three numbers.\n"));
+let numberTwo = Number(readlineSync.question(""))
+let numberThree = Number(readlineSync.question(""))
+
+if (Number.isNaN(numberOne || numberTwo || numberThree)) {
+    console.log("Invalid");
+} else if (numberOne < MIN || numberOne > MAX || numberTwo < MIN || numberTwo > MAX || numberThree < MIN || numberThree > MAX) {
+    console.log("Invalid");
+} else if (((numberOne - Math.floor(numberOne)) !==0) || ((numberTwo - Math.floor(number2)) !==0) || ((numberThree - Math.floor(number3)) !==0)) {
+    console.log("\nInvalid.");
+} else if (numberOne < numberTwo && numberTwo < numberThree) {
+    console.log("\nStrictly increasing.")
+} else if (numberOne > numberTwo && numberTwo> numberThree) {
+    console.log("\nStrictly decreasing.")
+} else if (numberOne < numberTwo && numberTwo == numberThree || numberOne == numberTwo && numberTwo < numberThree) {
+    console.log("\nIncreasing.")
+} else if (numberOne == numberTwo && numberTwo > numberThree || numberOne > numberTwo && numberTwo == numberThree) {
+    console.log("\nDecreasing.")
+} else if (numberOne == numberTwo && numberTwo == numberThree) {
+    console.log("\nEqual.")
+} else {
+    console.log("\nUnordered.")
+}
