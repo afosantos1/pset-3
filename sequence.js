0b8a4325b9c2869be74404f73022b0b5fbac8855
Afonso Santos <55449265+afosantos1@users.noreply.github.com>
2019-11-02 16:59:37 -0400

    Update sequence.js

--- sequence.js
+++ sequence.js
@@ -1,27 +1,29 @@
 const readlineSync = require("readline-sync");
 
-const MIN = Number.MIN_SAFE_INTEGER;
-const MAX = Number.MAX_SAFE_INTEGER;
-let numberOne = Number(readlineSync.question("\nEnter three numbers.\n"));
-let numberTwo = Number(readlineSync.question(""))
-let numberThree = Number(readlineSync.question(""))
+console.log("\nEnter three numbers. \n");
+const numberOne = Number(readlineSync.question(""));
+const numberTwo = Number(readlineSync.question(""));
+const numberThree = Number(readlineSync.question(""));
 
-if (Number.isNaN(numberOne || numberTwo || numberThree)) {
-    console.log("Invalid");
-} else if (numberOne < MIN || numberOne > MAX || numberTwo < MIN || numberTwo > MAX || numberThree < MIN || numberThree > MAX) {
-    console.log("Invalid");
-} else if (((numberOne - Math.floor(numberOne)) !==0) || ((numberTwo - Math.floor(numberTwo)) !==0) || ((numberThree - Math.floor(numberThree)) !==0)) {
+if (Number.isNaN(numberOne) || Number.isNaN(numberTwo) || Number.isNaN(numberThree)) {
     console.log("\nInvalid.");
-} else if (numberOne < numberTwo && numberTwo < numberThree) {
-    console.log("\nStrictly increasing.")
-} else if (numberOne > numberTwo && numberTwo> numberThree) {
-    console.log("\nStrictly decreasing.")
-} else if (numberOne < numberTwo && numberTwo == numberThree || numberOne == numberTwo && numberTwo < numberThree) {
-    console.log("\nIncreasing.")
-} else if (numberOne == numberTwo && numberTwo > numberThree || numberOne > numberTwo && numberTwo == numberThree) {
-    console.log("\nDecreasing.")
-} else if (numberOne == numberTwo && numberTwo == numberThree) {
-    console.log("\nEqual.")
-} else {
-    console.log("\nUnordered.")
-}
+} else if (numberOne < Number.MIN_SAFE_INTEGER || numberOne > Number.MAX_SAFE_INTEGER || numberTwo < Number.MIN_SAFE_INTEGER || secondNumber > Number.MAX_SAFE_INTEGER || thirdNumber < Number.MIN_SAFE_INTEGER || thirdNumber > Number.MAX_SAFE_INTEGER) {
+    console.log("\nInvalid.");
+} else if (numberOne<numberTwo && numberTwo<numberThree) {
+      console.log("\nStrictly increasing.")
+    }
+  else if (numberOne==numberTwo && numberTwo==numberThree) {
+      console.log("\nEqual.")
+    }
+  else if (numberOne>numberTwo && numberTwo>numberThree) {
+      console.log("\nStrictly decreasing.")
+    }
+  else if (numberOne<=numberTwo && numberTwo<=numberThree) {
+      console.log("\nIncreasing.")
+    }
+  else if (numberOne>=numberTwo && numberTwo>=numberThree) {
+      console.log("\nDecreasing.")
+    }
+  else {
+      console.log("\nUnordered.")
+    }
